0f67b779ed78830aff2f905199fb48f1686e23fd
Chacaltheonly <95366778+Chacaltheonly@users.noreply.github.com>
2026-03-25 16:56:13 -0300

    Configure published Sheets endpoint

--- sheets-config.js
+++ sheets-config.js
@@ -1,5 +1,6 @@
 window.LIVESELL_CONFIG = {
-  sheetsEndpoint: "",
+  sheetsEndpoint:
+    "https://script.google.com/macros/s/AKfycbzGFiEdAElHjUIGjzm-OrzOatDVMS7Wf_qL_shvF2jRAXA7MOIv6xxtZ4tgD5CJHoem/exec",
   stockSheetName: "Estoque",
   salesSheetName: "Vendas",
   syncDebounceMs: 700,
